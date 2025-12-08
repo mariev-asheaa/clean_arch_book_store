14e0640f5dabef77500a537d1f7e772ea3f9b0f1
marievasheaa <marievaheaa170@gmail>
2025-12-08 02:30:18 +0200

    add either using dartz

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,8 +1,10 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-  Future<List<BookEntity>>fetchFeaturedBooks();
-  Future<List<BookEntity>>fetchNewestBooks();
+  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks();
+  Future<Either<Failure,List<BookEntity>>>fetchNewestBooks();
 }
 
 //step 2 in clean arch
