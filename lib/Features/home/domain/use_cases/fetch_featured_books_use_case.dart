9cebbcf033518d0917a4b264b60ca7866c1d1d83
marievasheaa <marievaheaa170@gmail>
2025-12-08 02:30:38 +0200

    add fetch_featured_books_use_case

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/book_entity.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks(){
+    //check user permission
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
