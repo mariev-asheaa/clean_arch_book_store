acf4fe7f2b997b948a9bcf68a3925500bac984f7
marievasheaa <marievaheaa170@gmail>
2025-12-11 01:58:31 +0200

    implement fetchFeaturedBooks

---
+++ lib/Features/home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,33 @@
+import 'package:bookly/Features/home/data/data_sources/home_local_data_sources.dart';
+import 'package:bookly/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplementation extends HomeRepo{
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImplementation({required this.homeRemoteDataSource, required this.homeLocalDataSource});
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks()async{
+    try {
+      var booksList=homeLocalDataSource.fetchFeaturedBooks();
+    if(booksList.isNotEmpty){
+      return right(booksList);
+    }
+    var books=await homeRemoteDataSource.fetchFeaturedBooks();
+    return right(books);
+    }catch (e) {
+       return left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+
+}
