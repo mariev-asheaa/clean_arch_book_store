529ed48cf3cadbc00ad1b9cc83c6ae70d6c733eb
marievasheaa <marievaheaa170@gmail.com>
2025-12-11 22:35:50 +0200

    create newest_books_state

---
+++ lib/Features/home/presentation/cubits/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,20 @@
+part of 'newest_books_cubit.dart';
+
+@immutable
+ class NewestBooksState {}
+
+ class NewestBooksInitial extends NewestBooksState {}
+
+class NewestBooksLoading extends NewestBooksState {}
+class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity>books;
+
+  NewestBooksSuccess({required this.books});
+
+}
+class NewestBooksFailure extends NewestBooksState {
+  final String errorMessage;
+
+  NewestBooksFailure({required this.errorMessage});
+
+}
