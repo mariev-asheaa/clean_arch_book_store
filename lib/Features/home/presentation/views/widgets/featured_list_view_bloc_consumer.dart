524fd1b48dd9d2c5faacc561b56d2558ef1d9434
marievasheaa <marievaheaa170@gmail.com>
2025-12-13 02:45:47 +0200

    show snack bar in pagination failure

--- lib/Features/home/presentation/views/widgets/featured_list_view_bloc_consumer.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view_bloc_consumer.dart
@@ -3,6 +3,7 @@ import 'package:bookly/Features/home/presentation/views/widgets/featured_list_vi
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/functions/build_error_snack_bar.dart';
 import '../../../domain/entities/book_entity.dart';
 
 class FeaturedListViewBlocConsumer extends StatefulWidget {
@@ -21,9 +22,14 @@ class _FeaturedListViewBlocConsumerState extends State<FeaturedListViewBlocConsu
       if (state is FeaturedBooksSuccess) {
         books.addAll(state.books);
       }
+      if(state is PaginationFailure){
+        ScaffoldMessenger.of(context).showSnackBar(
+          buildErrorWidget(state.errorMessage),
+        );
+      }
       },
       builder: (context, state) {
-        if(state is FeaturedBooksSuccess||state is PaginationLoading){
+        if(state is FeaturedBooksSuccess||state is PaginationLoading||state is PaginationFailure){
           return FeaturedBooksListView(books: books,);
         }
      else if(state is FeaturedBooksFailure){
